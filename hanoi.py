ee037f9de62fabf3e246b298dddf84a0a84890f4
John Kelly <jkelly1@godaddy.com>
2022-08-22 17:01:31 +0100

    Towers of Hanoi
    This is the initial commit.
    I need to sort out some stuff still.

---
+++ hanoi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+'''
+hanoi.py 
+  script to solve the tower of Hanoi problem
+'''
+
+
+def moveDisk(A,B) :
+    '''
+    Simple moveDisk routine.
+    '''
+    print("Move disk from {} to {}".format(A,B))
+
+
+def solveHanoi(n,f,h,t) :
+    '''
+    Solve the problem - Recursively
+    '''
+    if n > 0:
+        solveHanoi(n-1, f, t, h)
+        moveDisk(f,t)
+        solveHanoi(n-1, h, f, t)
+    else: 
+        pass
+
+if __name__ == "__main__" :
+#    moveDisk("R","K")
+    solveHanoi(4, "F", "H", "T")
+
+
